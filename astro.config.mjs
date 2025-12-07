d5816611dd3f8c925afa15acef1f3a7e5be5bc18
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2025-12-07 14:00:57 +0100

    mise à jour dépendances

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 
 import vue from "@astrojs/vue";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()],
+  integrations: [vue()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
